15eb87b9e234c4a5dad2e9192da3c488df5da510
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-04-11 22:21:45 +0100

    add solutions for writing-tests section

--- writing-tests/largest-number/largest-number.test.js
+++ writing-tests/largest-number/largest-number.test.js
@@ -1,9 +1,12 @@
 let getLargestNumber = require("./largest-number");
 
-test("returns largest number in array", function () {
-  // Arrange
-  // Act
-  // Assert
+test("returns largest number in array", function() {
+  const input = [3, 21, 88, 4, 36];
+  const output = 88;
+
+  const largestNumber = getLargestNumber(input);
+
+  expect(output).toEqual(largestNumber);
 });
 
 // example
--- writing-tests/remove-vowels/remove-vowels-in-array.test.js
+++ writing-tests/remove-vowels/remove-vowels-in-array.test.js
@@ -1,9 +1,12 @@
 let removeVowelsFromWords = require("./remove-vowels-in-array");
 
-test("remove vowels from all words in array", function () {
-  // Arrange
-  // Act
-  // Assert
+test("remove vowels from all words in array", function() {
+  const input = ["Irina", "Etza", "Daniel"];
+  const output = ["rn", "tz", "Dnl"];
+
+  const wordsWithoutVowels = removeVowelsFromWords(input);
+
+  expect(output).toEqual(wordsWithoutVowels);
 });
 
 // example
