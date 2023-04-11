12e32a73cea6325447cbe6ac3f6a43c9ca4c8287
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-04-11 22:04:49 +0100

    add solution for array destructuring week 3

--- array-destructuring/exercise-3/exercise.js
+++ array-destructuring/exercise-3/exercise.js
@@ -1,8 +1,27 @@
-let order = [
-  { itemName: "Hot cakes", quantity: 1, unitPrice: 2.29 },
-  { itemName: "Apple Pie", quantity: 2, unitPrice: 1.39 },
-  { itemName: "Egg McMuffin", quantity: 1, unitPrice: 2.8 },
-  { itemName: "Sausage McMuffin", quantity: 1, unitPrice: 3.0 },
-  { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
-  { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
+const order = [
+  { itemName: "Hot cakes", quantity: 1, unitPrice: 2.29},
+  { itemName: "Apple Pie", quantity: 2, unitPrice: 1.39},
+  { itemName: "Egg McMuffin", quantity: 1, unitPrice: 2.80},
+  { itemName: "Sausage McMuffin", quantity: 1, unitPrice: 3.00},
+  { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.00},
+  { itemName: "Hash Brown", quantity: 4, unitPrice: 0.40}
 ];
+
+function pad(value, width) {
+  return value.toString().padEnd(width, " ");
+}
+
+function printRow(quantity, itemName, totalPrice) {
+  console.log(`${pad(quantity, 7)} ${pad(itemName, 19)} ${totalPrice}`);
+}
+
+let total = 0;
+printRow("QTY", "ITEM", "TOTAL");
+
+for (const {quantity, itemName, unitPrice} of order) {
+  printRow(quantity, itemName, (unitPrice * quantity).toFixed(2));
+  total += unitPrice * quantity;
+}
+
+
+console.log(`Total: ${total}`);
