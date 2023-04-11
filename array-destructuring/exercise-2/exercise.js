a65e5392603c42f5ac5f412c03756dda4ec7dc64
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-04-11 22:03:04 +0100

    add exercises for array destructuring week 2

--- array-destructuring/exercise-2/exercise.js
+++ array-destructuring/exercise-2/exercise.js
@@ -1,72 +1,32 @@
-let hogwarts = [
-  {
-    firstName: "Harry",
-    lastName: "Potter",
-    house: "Gryffindor",
-    pet: "Owl",
-    occupation: "Student",
-  },
-  {
-    firstName: "Ron",
-    lastName: "Weasley",
-    house: "Gryffindor",
-    pet: "Scabbers",
-    occupation: "Student",
-  },
-  {
-    firstName: "Hermione",
-    lastName: "Granger",
-    house: "Gryffindor",
-    pet: "Cat",
-    occupation: "Student",
-  },
-  {
-    firstName: "Draco",
-    lastName: "Malfoy",
-    house: "Slytherin",
-    pet: null,
-    occupation: "Student",
-  },
-  {
-    firstName: "Cedric",
-    lastName: "Diggory",
-    house: "HufflePuff",
-    pet: null,
-    occupation: "Student",
-  },
-  {
-    firstName: "Severus",
-    lastName: "Snape",
-    house: "Slytherin",
-    pet: null,
-    occupation: "Teacher",
-  },
-  {
-    firstName: "Filius",
-    lastName: "Flitwick",
-    house: "Ravenclaw",
-    pet: null,
-    occupation: "Teacher",
-  },
-  {
-    firstName: "Pomona",
-    lastName: "Sprout",
-    house: "Hufflepuff",
-    pet: null,
-    occupation: "Teacher",
-  },
-  {
-    firstName: "Minerva",
-    lastName: "McGonagall",
-    house: "Gryffindor",
-    pet: null,
-    occupation: "Teacher",
-  },
-  {
-    firstName: "Albus",
-    lastName: "Dumbledore",
-    house: "Gryffindor",
-    pet: "Phoenix",
-    occupation: "Teacher",
-  },
+const hogwarts = [
+  { firstName: "Harry", lastName: "Potter", house: "Gryffindor", pet: "Owl", occupation: "Student" },
+  { firstName: "Ron", lastName: "Weasley", house: "Gryffindor", pet: "Scabbers", occupation: "Student" },
+  { firstName: "Hermione", lastName: "Granger", house: "Gryffindor", pet: "Cat", occupation: "Student" },
+  { firstName: "Draco", lastName: "Malfoy", house: "Slytherin", pet: null, occupation: "Student" },
+  { firstName: "Cedric", lastName: "Diggory", house: "HufflePuff", pet: null, occupation: "Student" },
+  { firstName: "Severus", lastName: "Snape", house: "Slytherin", pet: null, occupation: "Teacher" },
+  { firstName: "Filius", lastName: "Flitwick", house: "Ravenclaw", pet: null, occupation: "Teacher" },
+  { firstName: "Pomona", lastName: "Sprout", house: "Hufflepuff", pet: null, occupation: "Teacher" },
+  { firstName: "Minerva", lastName: "McGonagall", house: "Gryffindor", pet: null, occupation: "Teacher" },
+  { firstName: "Albus", lastName: "Dumbledore", house: "Gryffindor", pet: "Phoenix", occupation: "Teacher" }
 ];
+
+// Task 1
+
+for (const {firstName, lastName, house} of hogwarts) {
+  if (house === "Gryffindor") {
+      console.log(`${firstName} ${lastName}`);
+  }
+}
+
+// Or using filter and forEach:
+
+hogwarts
+  .filter(({house}) => house === "Gryffindor")
+  .forEach(({firstName, lastName}) => console.log(`${firstName} ${lastName}`));
+
+// Task 2
+
+hogwarts
+  .filter(({occupation, pet}) => occupation === "Teacher" && pet)
+  .forEach(({firstName, lastName}) => console.log(`${firstName} ${lastName}`));
