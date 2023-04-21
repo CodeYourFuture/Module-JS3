b30a61bbea4b175a751675ca01f166522031f6b4
Shahid Amin <schboostie@gmail.com>
2023-04-21 16:21:06 +0100

    Exercises  2 - 3 and books

--- array-destructuring/exercise-3/exercise.js
+++ array-destructuring/exercise-3/exercise.js
@@ -6,3 +6,22 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+
+
+function orderReceipt(){
+
+let totalCost = 0;
+
+order.forEach(({ itemName, quantity, unitPrice }) => {
+  const itemTotal = quantity * unitPrice;
+  totalCost += itemTotal;
+
+  console.log(`${quantity} ${itemName} @ ${unitPrice.toFixed(2)} = ${itemTotal.toFixed(2)}`);
+});
+
+return `Total: ${totalCost.toFixed(2)}`;
+
+}
+
+orderReceipt()
