bce24809a6de5f41bbcbe6b5a13c929ac42e426a
AnnaFYZ <113105718+AnnaFYZ@users.noreply.github.com>
2023-05-09 21:21:25 +0100

    2 exercises

--- array-destructuring/exercise-2/exercise.js
+++ array-destructuring/exercise-2/exercise.js
@@ -70,3 +70,15 @@ let hogwarts = [
     occupation: "Teacher",
   },
 ];
+
+// task 1
+let gryffindorHouse = hogwarts.filter(({house}) => house === "Gryffindor");
+gryffindorHouse.forEach(({firstName, lastName}) => console.log(`${firstName} ${lastName}`));
+
+// task 2
+
+let whoHavePets = hogwarts.filter(
+  ({ occupation, pet }) => occupation === "Teacher" && pet);
+whoHavePets.forEach(({ firstName, lastName }) =>
+  console.log(`${firstName} ${lastName}`)
+);
