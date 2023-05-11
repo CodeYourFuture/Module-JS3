9116e458d0af5f6d9d25fe64e070612d2dbd1e09
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-05-11 20:56:50 +0100

    fixed the submit function

--- book-library/script.js
+++ book-library/script.js
@@ -37,8 +37,8 @@ function submit() {
     alert("Please fill all fields!");
     return false;
   } else {
-    let book = new Book(title.value, title.value, pages.value, check.checked);
-    library.push(book);
+    let book = new Book(title.value, author.value, pages.value, check.checked);
+    myLibrary.push(book);
     render();
   }
 }
