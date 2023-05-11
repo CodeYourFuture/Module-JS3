47862384521bafe1a0fcb98b24ce7a6f96720060
AnnaFYZ <113105718+AnnaFYZ@users.noreply.github.com>
2023-05-11 21:04:24 +0100

    exersise 3

--- array-destructuring/exercise-3/exercise.js
+++ array-destructuring/exercise-3/exercise.js
@@ -6,3 +6,33 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+// QTY     ITEM                TOTAL
+// 1       Hot Cakes           2.29
+// 2       Apple Pie           2.78
+// 1       Egg McMuffin        2.80
+// 1       Sausage McMuffin    3.00
+// 2       Hot Coffee          2.00
+// 4       Hash Brown          1.60
+
+// Total: 14.47
+
+let {itemName, quantity, unitPrice} = order;
+
+function printOut() {
+  let total = 0;
+  order.forEach(({quantity, unitPrice}) => {
+      total += (quantity*unitPrice)
+  })
+
+  console.log(`${"QTY".padEnd(8)}${"ITEM".padEnd(20)}${"TOTAL"}`);
+
+  order.forEach(({quantity, itemName, unitPrice}) => {
+    console.log(`${quantity.toString().padEnd(8)}${itemName.padEnd(20)}${unitPrice.toFixed(2)}`)
+  })
+  console.log(" ")
+  console.log(`TOTAL: ${total.toFixed(2)}`);
+  return total
+}
+
+printOut();
