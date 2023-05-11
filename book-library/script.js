53c2e10ab8fe0beb5df662d0df49244d19a98fc9
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-05-11 10:02:45 +0100

    Added title and amended some errors
    Changed type values.
    Added a for value to checkbox label.
    Removed a semi colon from onclick=submit()

--- book-library/script.js
+++ book-library/script.js
@@ -29,16 +29,16 @@ const check = document.getElementById("check");
 //via Book function and start render function
 function submit() {
   if (
-    title.value == null ||
-    title.value == "" ||
-    pages.value == null ||
-    pages.value == ""
+    title.value === null ||
+    title.value === "" ||
+    pages.value === null ||
+    pages.value === ""
   ) {
     alert("Please fill all fields!");
     return false;
   } else {
     let book = new Book(title.value, title.value, pages.value, check.checked);
-    library.push(book);
+    myLibrary.push(book);
     render();
   }
 }
@@ -54,7 +54,7 @@ function render() {
   let table = document.getElementById("display");
   let rowsNumber = table.rows.length;
   //delete old table
-  for (let n = rowsNumber - 1; n > 0; n-- {
+  for (let n = rowsNumber - 1; n > 0; n--) {
     table.deleteRow(n);
   }
   //insert updated row and cells
