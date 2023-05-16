fcbc1869c4133a65ddfb60e06f0ae032ab0e4ac8
Olha Danylevska <odanilevskaya2@gmail.com>
2023-05-16 22:36:54 +0100

    dog-Photo-Gallery

--- dog-photo-gallery/script.js
+++ dog-photo-gallery/script.js
@@ -1,5 +1,37 @@
-async function myPhotoData() {
-  let response = await fetch("https://dog.ceo/api/breeds/image/random");
-  let data = await response.json();
-  console.log(data);
+function createPhoto(object){
+    let listOfPhotos = document.querySelector("#photo-gallery")
+    let randomList = document.createElement("li")
+    randomList.className = "list-element"
+    let randomPicture = document.createElement("img")
+    randomPicture.className ="image"
+    let randomPictureURL = object.message
+    randomPicture.src = randomPictureURL
+
+    listOfPhotos.appendChild(randomList)
+    randomList.appendChild(randomPicture)
 }
+let addButton = document.querySelector("button")
+    if(addButton){
+        addButton.addEventListener("click", fetchPhotoData)
+    } else{
+        console.log("Error") 
+    }
+
+
+function fetchPhotoData (){
+fetch(`https://dog.ceo/api/breeds/image/random`)
+    .then((response) => {
+      if (response.ok){
+        return response.json();
+      } else {
+        console.log("Error") 
+      }
+    })
+    .then((data) => {
+      createPhoto(data)
+    })   
+}
+
+
+
+
