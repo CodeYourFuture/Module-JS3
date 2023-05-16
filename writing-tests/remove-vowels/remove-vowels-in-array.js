d1ab771a8ff1decdeb848e90864851c4e2a5c4f8
Shadi38 <113238379+Shadi38@users.noreply.github.com>
2023-05-16 21:43:20 +0100

    writing-tests completed

--- writing-tests/remove-vowels/remove-vowels-in-array.js
+++ writing-tests/remove-vowels/remove-vowels-in-array.js
@@ -1,13 +1,14 @@
 let removeVowels = require("./remove-vowels");
-
+let input = ["Irina", "Etza", "Daniel"];
 function removeVowelsFromWords(words) {
-  let result = words.map(function (word) {
+  let result = words.map(function (word) { 
+
     return removeVowels(word);
   });
 
   return result;
 }
-
+removeVowelsFromWords(input);
 module.exports = removeVowelsFromWords;
 
 /*
