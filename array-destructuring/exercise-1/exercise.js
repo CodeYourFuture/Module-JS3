96c32ad72d92230ce21fc2a588788963a7ee8412
HereshT <hiresh.turky@gmail.com>
2023-05-16 13:27:59 +0100

    array-destructuring/exercise-1

--- array-destructuring/exercise-1/exercise.js
+++ array-destructuring/exercise-1/exercise.js
@@ -3,11 +3,13 @@ const personOne = {
   age: 34,
   favouriteFood: "Spinach",
 };
+let { name, age, favouriteFood } = personOne;
 
-function introduceYourself(___________________________) {
+function introduceYourself({ name, age, favouriteFood }) {
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
 }
 
 introduceYourself(personOne);
+
