d02b8791c315bf1826dfcd1f11be1681f4b18a15
softacoder <113185323+softacoder@users.noreply.github.com>
2023-05-17 13:19:11 +0100

    Update

--- using-fetch/exercise.js
+++ using-fetch/exercise.js
@@ -17,10 +17,28 @@ Open index.html in your browser. Every time you refresh the page,
 a different greeting should be displayed in the box.
 */
 
-fetch("*** Write the API address here ***")
+fetch("https://www.greetingsapi.com/random")
   .then(function (response) {
-    return response.text();
+    return response.json();
   })
-  .then(function (greeting) {
-    // Write the code to display the greeting text here
+  .then(function (greeting) { let greetText = document.querySelector(#greeting-text); 
+    greetText.innerText = `Hello: ${greeting.greeting}`;
+    greetText.innerText += `\nLanguage: ${greeting.language}`;
+    
   });
+
+// fetch("https://www.greetingsapi.com/random")
+//   .then( (response) => {
+//     return response.json();
+//   })
+
+//   .then(function (result) {
+//     let greetingText = document.getElementById("greeting-text");
+//     greetingText.textContent=`${result.type.toUpperCase()} it is greeting by ${result.language} Language`
+//   })
+//   .catch(error => console.error("some thing wrong", error));
+
+  // I use fetch to call the API. This initiates an asynchronous request to the specifies URL. The .then() method is called 
+  // on the Response object. The .then() method is used to handle the result of a promise and takes a callback function as its argument. 
+  // I then return the response from json to have the promise returned by response.json(). 
+
