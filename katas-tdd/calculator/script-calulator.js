09e08586b7953855b622b469c6fcc5dbfe026947
Olha Danylevska <odanilevskaya2@gmail.com>
2023-05-18 11:22:00 +0100

    8

---
+++ katas-tdd/calculator/script-calulator.js
@@ -0,0 +1,22 @@
+
+function findSum(str){
+    if(str === ""){
+        return 0
+    }
+
+}
+findSum("")
+
+
+
+
+
+
+
+
+//tests
+
+test("When string is empty", () => {
+  let str = ""
+  expect(findSum("")).toEqual(0);
+});
