b69d1e94f1000b2bb858e5ff24e7a814f5afe016
Irianni Munoz <82213740+munozirianni1988@users.noreply.github.com>
2023-05-18 22:56:29 +0100

    Done
    Array destructuring exercise done

--- array-destructuring/exercise-2/exercise.js
+++ array-destructuring/exercise-2/exercise.js
@@ -70,3 +70,9 @@ let hogwarts = [
     occupation: "Teacher",
   },
 ];
+
+hogwarts.forEach(({ firstName, lastName, house }) => {
+  if (house === "Gryffindor") {
+    console.log(`${firstName} ${lastName}`);
+  }
+});
