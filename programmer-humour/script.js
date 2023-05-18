bb08b9e1032d3d612b4b0448cd121c9a067d89bb
nasir ali <a.nasirali@outlook.com>
2023-05-18 12:14:12 +0100

    testing fetch

---
+++ programmer-humour/script.js
@@ -0,0 +1,9 @@
+// Function to make API call and fetch the latest xkcd comic
+function fetchLatestComic() {
+    fetch('https://xkcd.now.sh/?comic=latest')
+      
+  }
+  
+  // Call the function to fetch and render the latest comic
+  fetchLatestComic();
+  
