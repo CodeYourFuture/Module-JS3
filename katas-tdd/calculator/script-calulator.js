2815c68e88093a2afa2b5c0928fb28b6b3f9fd1f
Olha Danylevska <odanilevskaya2@gmail.com>
2023-05-18 13:22:48 +0100

    calculator

--- katas-tdd/calculator/script-calulator.js
+++ katas-tdd/calculator/script-calulator.js
@@ -1,14 +1,23 @@
 
+
 function findSum(str){
     if(str === ""){
         return 0
+    } else if(str !== "" && str.length === 1){
+      return parseInt(str)
+    } else{
+      let newArray = str.split(",")
+      let sum = 0
+      for (let item of newArray){
+        if((parseInt(item))< 0){
+          throw new Error (`negatives not allowed: ${item}`)  
+        }else if (! isNaN(item) && (parseInt(item))<1000 && (parseInt(item))>=0 ){
+          sum += parseInt(item)
+        } 
+      }
+      return sum
     }
-
 }
-findSum("")
-
-
-
 
 
 
@@ -17,6 +26,21 @@ findSum("")
 //tests
 
 test("When string is empty", () => {
-  let str = ""
   expect(findSum("")).toEqual(0);
+});
+
+test("When string has 1 number", () => {
+  expect(findSum("1")).toEqual(1);
+});
+
+test("When string has more then 1 number", () => {
+  expect(findSum("1,5")).toEqual(6);
+});
+
+test("ignore the numbers which bigger then 1000", () => {
+  expect(findSum("6,1001")).toEqual(6);
+});
+
+test("If the numbers is negative", () => {
+  expect(findSum("6,5,-1")).toThrow(new Error("negatives not allowed: -1"));
 });
