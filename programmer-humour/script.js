ac998cf6e77d0e8fdaa050362246de9f4b7a8f1c
nasir ali <a.nasirali@outlook.com>
2023-05-18 12:14:41 +0100

    added fetch functions

--- programmer-humour/script.js
+++ programmer-humour/script.js
@@ -1,6 +1,12 @@
 // Function to make API call and fetch the latest xkcd comic
 function fetchLatestComic() {
     fetch('https://xkcd.now.sh/?comic=latest')
+      .then(response => response.json())
+      .then(data => {
+        const comicImage = document.getElementById('comic-image');
+   
+        console.log(data);
+      })
       
   }
   
