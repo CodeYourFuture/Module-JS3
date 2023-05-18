311a542036fc51b41cd000c7303fbe680dff9bce
nasir ali <a.nasirali@outlook.com>
2023-05-18 13:14:37 +0100

    working on excersie 3

--- array-destructuring/exercise-3/exercise.js
+++ array-destructuring/exercise-3/exercise.js
@@ -6,3 +6,14 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+ 
+
+// Task 1: Display the names of people who belong to Gryffindor house
+console.log(`each individual total cost of the order
+'''
+QTY     ITEM                TOTAL
+`);
+for (const { itemName,quantity ,unitPrice } of order) {
+  console.log(`${quantity}     ${itemName}                ${unitPrice*quantity}`);
+
+}
