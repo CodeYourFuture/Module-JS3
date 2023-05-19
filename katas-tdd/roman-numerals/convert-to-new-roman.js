5f4d12489f7220d811edb9cc82a0011ca18ae604
Olha Danylevska <odanilevskaya2@gmail.com>
2023-05-19 14:17:47 +0100

    convert to new roman done

--- katas-tdd/roman-numerals/convert-to-new-roman.js
+++ katas-tdd/roman-numerals/convert-to-new-roman.js
@@ -1,3 +1,31 @@
-function convertToNewRoman(n) {}
+function convertToNewRoman(n) {
+    if(n===0) {
+            return ""
+    }   else if(n>=1000) {
+                return "M" + convertToNewRoman(n - 1000)
+        }   else if(n>=900) {
+                    return "CM" + convertToNewRoman(n - 900)
+            }   else if(n>=500) {
+                        return "D" + convertToNewRoman(n - 500)
+                }   else if(n>=400) {
+                            return "CD" + convertToNewRoman(n - 400)
+                    }   else if(n>=100) {
+                                return "C" + convertToNewRoman(n - 100)
+                        }   else if(n>=90) {
+                                return "XC" + convertToNewRoman(n - 90)
+                            }   else if(n >= 40){
+                                    return "XL" + convertToNewRoman(n - 40)
+                                }   else if(n >= 10){
+                                        return "X" + convertToNewRoman(n - 10)
+                                    }   else if(n >= 9){
+                                        return "IX" + convertToNewRoman(n - 9)
+                                        }   else if(n >= 5){
+                                                return "V" + convertToNewRoman(n - 5)
+                                            }   else if(n >= 4){
+                                                    return "IV" + convertToNewRoman(n - 4)
+                                                }   else if(n >= 1){
+                                                        return "I" + convertToNewRoman(n - 1)
+                                                    }
+}
 
 module.exports = convertToNewRoman;
--- katas-tdd/roman-numerals/convert-to-new-roman.test.js
+++ katas-tdd/roman-numerals/convert-to-new-roman.test.js
@@ -1,7 +1,33 @@
 let convertToNewRoman = require("./convert-to-new-roman");
 
 test("returns I if passed 1 as an argument", function () {
-  // Arrange
-  // Act
-  // Assert
+  expect(convertToNewRoman(1)).toEqual("I");
+});
+
+test("returns IV if passed 4  as an argument", function () {
+  expect(convertToNewRoman(4)).toEqual("IV");
+});
+
+test("returns IX if passed 9  as an argument", function () {
+  expect(convertToNewRoman(9)).toEqual("IX");
+});
+
+test("returns XIV if passed 14  as an argument", function () {
+  expect(convertToNewRoman(14)).toEqual("XIV");
+});
+
+test("returns XLIV if passed 44  as an argument", function () {
+  expect(convertToNewRoman(44)).toEqual("XLIV");
+});
+
+test("returns XCIX if passed 99  as an argument", function () {
+  expect(convertToNewRoman(99)).toEqual("XCIX");
+});
+
+test("returns CD if passed 400  as an argument", function () {
+  expect(convertToNewRoman(400)).toEqual("CD");
+});
+
+test("returns CMXLIV if passed 944  as an argument", function () {
+  expect(convertToNewRoman(944)).toEqual("CMXLIV");
 });
