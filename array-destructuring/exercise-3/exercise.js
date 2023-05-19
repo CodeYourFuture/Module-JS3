be7289ea36e15583d673104874a9c9ca9ab45cbf
softacoder <113185323+softacoder@users.noreply.github.com>
2023-05-19 15:25:15 +0100

    Update array-destructing

--- array-destructuring/exercise-3/exercise.js
+++ array-destructuring/exercise-3/exercise.js
@@ -6,3 +6,15 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+function printOrder (order) {
+  console.log(`Quantity Item Total`)
+  let total = 0;
+  order.forEach((item)=>{
+  let {itemName, quantity, unitPrice} = item
+  console.log(itemName, quantity, (unitPrice*quantity).toFixed(2));
+  total = total + (unitPrice * quantity)
+  })
+  console.log(`Total: ${total}`)
+  }
+  
+  print order (order);
