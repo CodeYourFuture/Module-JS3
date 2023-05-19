c45394ce168c279cc25e4fe7f0613705c7495def
Man Sang Sin <man.sang.sin@gmail.com>
2023-05-19 15:45:08 +0100

    applied destructuring assignment of variables
    declared variables for personOne.name, personOne.age and personOne.favouriteFood

--- array-destructuring/exercise-1/exercise.js
+++ array-destructuring/exercise-1/exercise.js
@@ -3,6 +3,11 @@ const personOne = {
   age: 34,
   favouriteFood: "Spinach",
 };
+// is equivalent to:
+// const personOne.name = "Popeye"
+// const personOne.age = 34
+// const personOne.favouriteFood = "Spinach"
+let {name, age, favouriteFood} = personOne;
 
 function introduceYourself(___________________________) {
   console.log(
