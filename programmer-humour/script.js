59f1c71759613f5df390ab4ef5cf11a16d992f0e
Olha Danylevska <odanilevskaya2@gmail.com>
2023-05-20 16:40:22 +0100

    finished humor

--- programmer-humour/script.js
+++ programmer-humour/script.js
@@ -1,6 +1,6 @@
 let myData
 function fetchHumor(){
-fetch(`https://xkcd.now.sh/?comic=latest`)
+  return fetch(`https://xkcd.now.sh/?comic=latest`)
     .then((response) => {
       if (response.ok){
         return response.json();
@@ -10,21 +10,24 @@ fetch(`https://xkcd.now.sh/?comic=latest`)
     })
     .then((data) => {
         myData = data
+        
       console.log("data", myData)
     });   
 }
 fetchHumor()
- console.log("data1", myData)
 
 
 function createImage(){
-     let mainDiv = document.querySelector("mainDiv")
+     let mainDiv = document.querySelector("#mainDiv")
     let imageURL = myData.img
+
     let image = document.createElement("img")
     image.src = imageURL
     mainDiv.appendChild(image)
 }
 
-createImage(myData)
+setTimeout(createImage, 1000); 
+ console.log("data1", myData)
+
 
 
