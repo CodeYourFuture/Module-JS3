c3948bc987ee7251a5ca525a43a93490a4ffdf0a
christina-mifsud <christina.mifsud@gmail.com>
2023-05-20 16:06:28 +0100

    Updated exercise 3
    Used console.table instead of console.log with \t

--- array-destructuring/exercise-1/exercise.js
+++ array-destructuring/exercise-1/exercise.js
@@ -10,7 +10,8 @@ const personOne = {
 // let { name, age, favouriteFood } = personOne; // You don't need to write this
 
 function introduceYourself({ age, name, favouriteFood }) {
-  // Can they can be in jumbled up (not in order)?
+  // Can they can be in jumbled up (not in order)? YES. The order of attributes in an object is not important! In fact, Objects are unordered collections of key-value pairs.
+  // So you can put these pairs in any order you want! In functions like the one in line 12, the order of input arguments is important, but in this case the function has just one input which it's type is object!
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
