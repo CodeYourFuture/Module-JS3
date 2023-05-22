978b6d3442694ce128736f19ae2f10f6cf2fbda2
AppolinFotso <appolin.s.fotso@gmail.com>
2023-05-22 21:40:40 +0100

    completed exercise 1

--- array-destructuring/exercise-1/exercise.js
+++ array-destructuring/exercise-1/exercise.js
@@ -4,7 +4,9 @@ const personOne = {
   favouriteFood: "Spinach",
 };
 
-function introduceYourself(___________________________) {
+let {name, age, favFood} = personOne
+function introduceYourself(person) {
+  let {name, age, favouriteFood} =person
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
