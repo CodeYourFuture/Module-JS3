8a1ed15ffb2246f0722481c5ec3a96de80286495
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-05-23 17:33:22 +0100

    exercise 1 and 2

--- array-destructuring/exercise-3/exercise.js
+++ array-destructuring/exercise-3/exercise.js
@@ -6,3 +6,5 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+order.forEach({itemName,quantity,unitPrice})
