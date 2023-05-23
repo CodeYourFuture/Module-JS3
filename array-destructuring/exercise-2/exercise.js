cb85684bd950dae426e6aa0fed1bcfb5b9b74cd5
filkovskyy <avfilkovsky@gmail.com>
2023-05-23 12:39:04 +0100

    removed all changes from prev commits

--- array-destructuring/exercise-2/exercise.js
+++ array-destructuring/exercise-2/exercise.js
@@ -70,30 +70,3 @@ let hogwarts = [
     occupation: "Teacher",
   },
 ];
-
-// Task 1
-function getGryffindorNames(array) {
-  const gryffindorNames = [];
-  for (const { firstName, lastName, house } of array) {
-    if (house === "Gryffindor") {
-      gryffindorNames.push(`${firstName} ${lastName}`);
-    }
-  }
-  return gryffindorNames;
-}
-
-console.log(getGryffindorNames(hogwarts));
-
-// Task 2
-
-function getTeacherPets(array) {
-  const teacherPets = [];
-  for (const { firstName, lastName, pet, occupation } of array) {
-    if (pet && occupation == "Teacher") {
-      teacherPets.push(`${firstName} ${lastName}`);
-    }
-  }
-  return teacherPets;
-}
-
-console.log(getTeacherPets(hogwarts));
