30e887a26687fb0a3b04602fbd21ecc76e1a5578
HereshT <hiresh.turky@gmail.com>
2023-05-23 11:08:17 +0100

    Revert "passing-tests Done"
    This reverts commit 2a2f198dda244fa07c723572eb91ae28033ea855.

--- passing-tests/car-sales/car-sales.js
+++ passing-tests/car-sales/car-sales.js
@@ -1,12 +1,3 @@
-function sales(carsSold) {
-  let totals = {};
-  for (let car of carsSold) {
-    if (!totals[car.make]) {
-      totals[car.make] = 0;
-    }
-    totals[car.make] += car.price;
-  }
-  return totals;
-}
+function sales(carsSold) {}
 
 module.exports = sales;
