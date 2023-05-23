ad5278b70a0f81779215d9ffe8a5e9d7a9de3764
HereshT <hiresh.turky@gmail.com>
2023-05-23 10:23:39 +0100

    passing-tests Done

--- passing-tests/factorial/factorial.js
+++ passing-tests/factorial/factorial.js
@@ -8,6 +8,10 @@
 // calculate and return the factorial of int
 // note: factorial of 0 is 1
 
-function factorial(int) {}
+function factorial(int) {
+  if (int < 0) return;
+  if (int === 0 || int === 1) return 1;
+  return int * factorial(int - 1);
+}
 
 module.exports = factorial;
--- passing-tests/get-average/get-average.js
+++ passing-tests/get-average/get-average.js
@@ -2,6 +2,16 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+  let sum = 0;
+  let count = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (typeof numbers[i] === "number") {
+      sum += numbers[i];
+      count++;
+    }
+  }
+  return sum / count;
+}
 
 module.exports = average;
