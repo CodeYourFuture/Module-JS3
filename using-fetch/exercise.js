438c3db77216dcddeedb1c69e754be31d97b5813
Bedi Omuri <113227370+Bedi06@users.noreply.github.com>
2023-05-24 12:07:32 +0100

    fetching done
    problem with fetching due to CORS policy: No 'Access-Control-Allow-Origin'

--- using-fetch/exercise.js
+++ using-fetch/exercise.js
@@ -17,10 +17,11 @@ Open index.html in your browser. Every time you refresh the page,
 a different greeting should be displayed in the box.
 */
 
-fetch("*** Write the API address here ***")
-  .then(function (response) {
+fetch("https://www.greetingsapi.com/random")
+  .then(function(response) {
     return response.text();
   })
-  .then(function (greeting) {
-    // Write the code to display the greeting text here
-  });
+  .then(function(greetings) {
+    document.getElementById("greeting-text").innerText = greetings;
+  })
+  .catch(error => console.error("Failed to load greeting", error));
