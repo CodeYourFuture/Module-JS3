10b713c0b67ec549b40de682d0ddafc62f823c67
Bedi Omuri <113227370+Bedi06@users.noreply.github.com>
2023-05-24 15:46:06 +0100

    programmer-hummor
    not-done yet

---
+++ programmer-humour/script.js
@@ -0,0 +1,18 @@
+// Function to make the API call and render the comic image
+async function fetchComic() {
+    try {
+      const response = await fetch('https://xkcd.now.sh/?comic=latest');
+      if (!response.ok) {
+        throw new Error('Failed to fetch the comic');
+      }
+      const data = await response.json();
+      console.log(data); // Log the received data to the console
+      const comicImage = document.getElementById('comic-img');
+      comicImage.src = data.img; 
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  window.addEventListener('load', fetchComic);
+  
