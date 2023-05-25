30840bcc6d9c99d6f90ec44b51c9c0c7b0946e0a
Man Sang Sin <man.sang.sin@gmail.com>
2023-05-25 07:47:51 +0100

    added function to build img element
    function creates an img element
    assigns data from img field of the object to the img src
    appends the element to the body of the html
    used window.onload to initiate function when page first loads

--- programmer-humour/script.js
+++ programmer-humour/script.js
@@ -3,4 +3,15 @@ fetch(`https://xkcd.now.sh/?comic=latest`)
   .then(function (response) {
     return response.json();
   })
+  .then(function (data) {
+    buildImgElement(data)
+  });
 }
+
+function buildImgElement(input) {
+    let imgElement = document.createElement("img");
+    imgElement.src = input.img
+    document.body.append(imgElement);
+}
+
+window.onload = callAPI
