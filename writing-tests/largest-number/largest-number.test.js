97b1891bd72d1bcecb61bc61a08704ec2d0873ab
christina-mifsud <christina.mifsud@gmail.com>
2023-05-25 23:06:34 +0100

    Writing tests exercises

--- writing-tests/largest-number/largest-number.test.js
+++ writing-tests/largest-number/largest-number.test.js
@@ -1,13 +1,22 @@
 let getLargestNumber = require("./largest-number");
-
-test("returns largest number in array", function () {
-  // Arrange
-  // Act
-  // Assert
-});
-
 // example
 // input: [3, 21, 88, 4, 36];
 // expected: 88;
 
+test("returns largest number in array", function () {
+  let array = [3, 21, 88, 4, 36];
+  let expected = 88;
+  let output = getLargestNumber(array);
+
+  expect(output).toEqual(expected);
+});
+
 // also test that the original array hasn't changed
+test("original array hasn't changed", function () {
+  let array = [3, 21, 88, 4, 36];
+  let arrayCopy = [...array];
+
+  getLargestNumber(array);
+
+  expect(array).toEqual(arrayCopy);
+});
--- writing-tests/remove-vowels/remove-vowels-in-array.test.js
+++ writing-tests/remove-vowels/remove-vowels-in-array.test.js
@@ -1,9 +1,12 @@
 let removeVowelsFromWords = require("./remove-vowels-in-array");
 
 test("remove vowels from all words in array", function () {
-  // Arrange
-  // Act
-  // Assert
+  let words = ["Irina", "Etza", "Daniel"];
+  let expected = ["rn", "tz", "Dnl"];
+
+  let output = removeVowelsFromWords(words);
+
+  expect(output).toEqual(expected);
 });
 
 // example
