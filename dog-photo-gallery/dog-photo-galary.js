88b3766efd45a064b4d90c40667bdec429a5cf7b
Saliha Popal <salihapopal@gmail.com>
2023-05-26 08:12:41 +0100

    Dog photo gallery

--- dog-photo-gallery/dog-photo-galary.js
+++ dog-photo-gallery/dog-photo-galary.js
@@ -1,31 +1,4 @@
-// const fetchButton = document.getElementById("fetchButton");
-// const gallery = document.getElementById("gallery");
-
-// fetchButton.addEventListener("click", fetchDogPhoto);
-
-// function fetchDogPhoto() {
-//   fetch("https://dog.ceo/api/breeds/image/random")
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error("Failed to fetch dog photo.");
-//       }
-//       return response.json();
-//     })
-//     .then(data => {
-//       const dogPhotoUrl = data.message;
-//       const listItem = document.createElement("li");
-//       const dogImage = document.createElement("img");
-//       dogImage.src = dogPhotoUrl;
-//       listItem.appendChild(dogImage);
-//       gallery.appendChild(listItem);
-//     })
-//     .catch(error => {
-//       console.log("Error:", error.message);
-//     });
-// }
 const fetchButton = document.getElementById("fetchButton");
-const gallery = document.getElementById("gallery");
-
 fetchButton.addEventListener("click", fetchDogPhoto);
 
 function fetchDogPhoto() {
@@ -37,14 +10,29 @@ function fetchDogPhoto() {
       return response.json();
     })
     .then(data => {
-      const dogPhotoUrl = data.message;
+      const ulElem = document.getElementById("gallery");
       const listItem = document.createElement("li");
+      listItem.id = "list-item";
+    
       const dogImage = document.createElement("img");
-      dogImage.src = dogPhotoUrl;
+      dogImage.id = "dog-img";
+      dogImage.src = data.message;
+    
+      ulElem.appendChild(listItem)
       listItem.appendChild(dogImage);
-      gallery.appendChild(listItem);
     })
     .catch(error => {
-      console.log("Error:", error.message);
+      console.log("There was a problem.", error);
     });
 }
+
+const clearGalleryBtn = document.getElementById("clearGalleryBtn");
+clearGalleryBtn.addEventListener("click", clearGallery);
+
+function clearGallery(){
+  const dogGallery = document.getElementById("gallery");
+  dogGallery.innerHTML = "";
+}
+  
+
+
