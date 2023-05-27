9a72a0598f851ceaa769bca81a86bfa12a0a3126
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-05-27 16:32:03 +0100

    added code for writing tests, largest number

--- writing-tests/largest-number/largest-number.test.js
+++ writing-tests/largest-number/largest-number.test.js
@@ -1,13 +1,23 @@
 let getLargestNumber = require("./largest-number");
 
+// Arrange
+// Act
+// Assert
 test("returns largest number in array", function () {
-  // Arrange
-  // Act
-  // Assert
+  let numbers = [3, 21, 88, 36];
+  let expected = 88;
+
+  let output = getLargestNumber(numbers);
+
+  expect(output).toEqual(expected);
 });
 
-// example
-// input: [3, 21, 88, 4, 36];
-// expected: 88;
-
 // also test that the original array hasn't changed
+test("original array hasn't changed", function () {
+  let numbers = [3, 21, 88, 36];
+  let copyOfNumbers = [...numbers];
+
+  getLargestNumber(numbers);
+
+  expect(numbers).toEqual(copyOfNumbers);
+});
