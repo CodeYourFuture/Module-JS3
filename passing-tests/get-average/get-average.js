361fc8f55ccdfe3b92819d06c676b79c8fbc7695
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-05-27 10:32:05 +0100

    added code for get-average

--- passing-tests/get-average/get-average.js
+++ passing-tests/get-average/get-average.js
@@ -1,7 +1,22 @@
 // the input is an array of numbers and strings
 // return the average of all the numbers
 // be sure to exclude the strings
+let numbers = [4, "-", 8, 11, "hello", "57", 0, 2];
 
-function average(numbers) {}
+function checkIfNumber(element) {
+  return typeof element === "number";
+}
+
+function average(numbers) {
+  let numberArray = numbers.filter(checkIfNumber);
+
+  // where a is the accumulator
+  // where b is the current value
+  const initialValue = 0;
+  const sumWithInitial = numberArray.reduce((a, b) => a + b, initialValue);
+
+  let averageOfNumberArray = sumWithInitial / numberArray.length;
+  return averageOfNumberArray;
+}
 
 module.exports = average;
