9435877c6dc2cddd5564f50d8708c6be9676c377
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-05-28 16:10:05 +0100

    dog photo Done
    add all files and sheet work
    
    create all element needs
    
    create function for the requirement

---
+++ dog-photo-gallery/dog.js
@@ -0,0 +1,29 @@
+
+
+// define variables 
+
+// const { doc } = require("prettier");
+
+let btn1 = document.getElementById('btn');
+// let btn2 = document.getElementById('bttn');
+let ulElement = document.getElementById('ul');
+
+// create EventListener to fetch and git data to display it 
+
+btn1.addEventListener('click', fetchingData)
+
+async function fetchingData(){
+    const response = await fetch("https://dog.ceo/api/breeds/image/random")
+.then((response) => {
+    if (response && response.ok) {
+        let data =  response.json()
+        return  data
+    }else {
+    throw new Error(response.statusText);
+    }
+})
+.then((data) => {
+    return ulElement.innerHTML = `<img src="${data.message}" >`
+})
+
+}
