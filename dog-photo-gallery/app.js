1468483605cd1b8ed4512e2b57f1e9711c7f71a8
seyyednavid hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-05-28 11:42:39 +0100

    Finishing programmer-humour assignment and refactor dog-photo-gallary

--- dog-photo-gallery/app.js
+++ dog-photo-gallery/app.js
@@ -1,5 +1,6 @@
 const buttonClick = document.querySelector("#btn-click");
 const list = document.querySelector("#list-img");
+let output = "";
 
 buttonClick.addEventListener("click", getApiResult);
 
@@ -7,9 +8,8 @@ function getApiResult() {
   fetch(`https://dog.ceo/api/breeds/image/random`)
     .then((response) => response.json())
     .then((data) => {
-      let output = "";
       output += `
-         <li style="list-style:none">
+         <li style="list-style:none" class="col-12 col-sm-5 col-md-3">
            <img src="${data.message}" style="height:250px; width:250px"></img>
          </li>
         `;
