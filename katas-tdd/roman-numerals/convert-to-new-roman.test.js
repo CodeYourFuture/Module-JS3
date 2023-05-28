db90489f171760c799746e9000bdd35eb4ba30fc
fowobi <109865505+fowobi@users.noreply.github.com>
2023-05-28 11:18:40 +0100

    roman-numerals done

--- katas-tdd/roman-numerals/convert-to-new-roman.test.js
+++ katas-tdd/roman-numerals/convert-to-new-roman.test.js
@@ -1,7 +1,20 @@
-let convertToNewRoman = require("./convert-to-new-roman");
+// let convertToNewRoman = require("./convert-to-new-roman");
+
+// test("returns I if passed 1 as an argument", function () {
+//   // Arrange
+//   // Act
+//   // Assert
+// });
+const convertToNewRoman = require("./convert-to-new-roman");
 
 test("returns I if passed 1 as an argument", function () {
   // Arrange
+  const number = 1;
+  const expected = "I";
+
   // Act
+  const result = convertToNewRoman(number);
+
   // Assert
+  expect(result).toEqual(expected);
 });
--- katas-tdd/roman-numerals/convert-to-old-roman.test.js
+++ katas-tdd/roman-numerals/convert-to-old-roman.test.js
@@ -1,7 +1,22 @@
-let convertToOldRoman = require("./convert-to-old-roman");
+// let convertToOldRoman = require("./convert-to-old-roman");
+
+// test("returns I if passed 1 as an argument", function () {
+//   // Arrange
+//   // Act
+//   // Assert
+// });
+
+
+const convertToOldRoman = require("./convert-to-old-roman");
 
 test("returns I if passed 1 as an argument", function () {
   // Arrange
+  const number = 1;
+  const expected = "I";
+
   // Act
+  const result = convertToOldRoman(number);
+
   // Assert
+  expect(result).toEqual(expected);
 });
