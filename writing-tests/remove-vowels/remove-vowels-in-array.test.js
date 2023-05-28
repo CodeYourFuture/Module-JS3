ca8cd1eb486e49f8d111b631f77e791339b4c3fa
Haroun Alarabi <alarabiharoun@gmail.com>
2023-05-28 15:57:14 +0100

    writting-test solved

--- writing-tests/remove-vowels/remove-vowels-in-array.test.js
+++ writing-tests/remove-vowels/remove-vowels-in-array.test.js
@@ -1,11 +1,7 @@
 let removeVowelsFromWords = require("./remove-vowels-in-array");
 
 test("remove vowels from all words in array", function () {
-  // Arrange
-  // Act
-  // Assert
-});
+  expect(removeVowelsFromWords(["Irina", "Etza", "Daniel"]))
+  .toStrictEqual(["rn", "tz", "Dnl"]);
 
-// example
-// input: ["Irina", "Etza", "Daniel"]
-// expected output: ["rn", "tz", "Dnl"]
+});
