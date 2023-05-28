26c9b6554ba9eedb71d78022e98262017c64d329
Haroun Alarabi <alarabiharoun@gmail.com>
2023-05-28 15:56:26 +0100

    passing test solved

--- passing-tests/car-sales/car-sales.js
+++ passing-tests/car-sales/car-sales.js
@@ -1,3 +1,25 @@
-function sales(carsSold) {}
+// function sales(carsSold) {
+//     return carsSold.map((car) => {
+//             let total =`${car.price} * ${car.quantity}`;
+//         return `${car.make }:,${total} `;
+//         });
+
+// }
+function sales(carsSold) {
+    let totals = {};
+
+    carsSold.forEach((car) => {
+      if (totals[car.make]) {
+        totals[car.make] += car.price;
+      }
+    else {
+        totals[car.make] = car.price ;
+      }
+    });
+
+    return totals;
+  }
 
 module.exports = sales;
+
+
--- passing-tests/get-average/get-average.js
+++ passing-tests/get-average/get-average.js
@@ -2,6 +2,16 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+   let total = 0 ,counter=0;
+    let length = numbers.length;
+    for (let i = 0; i < length; i++) {
+        if (typeof numbers[i] === "number") {
+            total += numbers[i]
+            counter++;
+            }
+        }
+            return total/counter;
+}
 
 module.exports = average;
