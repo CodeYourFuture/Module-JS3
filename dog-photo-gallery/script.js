7625264a598b9f7e9a91bc356a5022d28320e4e5
fowobi <109865505+fowobi@users.noreply.github.com>
2023-05-28 09:19:57 +0100

    update dog-photo-gallery

--- dog-photo-gallery/script.js
+++ dog-photo-gallery/script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 async function randomDogPicture (){
    try {
        const response = await fetch('https://dog.ceo/api/breeds/image/random');
@@ -24,4 +25,27 @@ button.addEventListener('click', async () => {
  const pictureUrl = await randomDogPicture();
  picture.src = pictureUrl;
  item.appendChild(picture);
+=======
+async function randomDogPicture() {
+  try {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random');
+    const json = await response.json();
+    return json.message;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+const button = document.getElementById('btn');
+button.addEventListener('click', async () => {
+  const list = document.getElementById('list');
+
+  const item = document.createElement('li');
+  list.appendChild(item);
+
+  const picture = document.createElement('img');
+  const pictureUrl = await randomDogPicture();
+  picture.src = pictureUrl;
+  item.appendChild(picture);
+>>>>>>> Stashed changes
 });
