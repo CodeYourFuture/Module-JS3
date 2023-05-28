55b48a9262a8e4f1d4081965903aaca7ffbb540c
fowobi <109865505+fowobi@users.noreply.github.com>
2023-05-28 10:13:54 +0100

    Update largest-number.test.js

--- writing-tests/largest-number/largest-number.test.js
+++ writing-tests/largest-number/largest-number.test.js
@@ -1,13 +1,29 @@
-let getLargestNumber = require("./largest-number");
+// let getLargestNumber = require("./largest-number");
 
-test("returns largest number in array", function () {
-  // Arrange
-  // Act
-  // Assert
-});
+// test("returns largest number in array", function () {
+//   // Arrange
+//   // Act
+//   // Assert
+// });
 
 // example
 // input: [3, 21, 88, 4, 36];
 // expected: 88;
 
 // also test that the original array hasn't changed
+
+const getLargestNumber = require('./largest-number');
+
+test('returns largest number in array', function () {
+  // Arrange
+  const array = [3, 21, 88, 4, 36];
+  const expected = 88;
+  const originalArray = [...array];
+
+  // Act
+  const result = getLargestNumber(array);
+
+  // Assert
+  expect(result).toEqual(expected);
+  expect(array).toEqual(originalArray); // Check that the original array hasn't changed
+});
