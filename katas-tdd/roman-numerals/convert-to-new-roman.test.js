7c6d1c535122c135ea97c3430b95ca9819698ed8
fowobi <109865505+fowobi@users.noreply.github.com>
2023-05-28 11:56:25 +0100

    roman-numeral update

--- katas-tdd/roman-numerals/convert-to-new-roman.test.js
+++ katas-tdd/roman-numerals/convert-to-new-roman.test.js
@@ -5,16 +5,27 @@
 //   // Act
 //   // Assert
 // });
-const convertToNewRoman = require("./convert-to-new-roman");
+const convertToNewRoman = require('./convert-to-new-roman');
 
-test("returns I if passed 1 as an argument", function () {
-  // Arrange
-  const number = 1;
-  const expected = "I";
-
-  // Act
-  const result = convertToNewRoman(number);
-
-  // Assert
-  expect(result).toEqual(expected);
+test('returns I if passed 1 as an argument', () => {
+  expect(convertToNewRoman(1)).toEqual('I');
 });
+
+test('returns IV if passed 4 as an argument', () => {
+  expect(convertToNewRoman(4)).toEqual('IV');
+});
+
+test('returns IX if passed 9 as an argument', () => {
+  expect(convertToNewRoman(9)).toEqual('IX');
+});
+
+test('returns XL if passed 40 as an argument', () => {
+  expect(convertToNewRoman(40)).toEqual('XL');
+});
+
+test('returns XC if passed 90 as an argument', () => {
+  expect(convertToNewRoman(90)).toEqual('XC');
+});
+
+
+
--- katas-tdd/roman-numerals/convert-to-old-roman.test.js
+++ katas-tdd/roman-numerals/convert-to-old-roman.test.js
@@ -7,16 +7,29 @@
 // });
 
 
-const convertToOldRoman = require("./convert-to-old-roman");
+const convertToOldRoman = require('./convert-to-old-roman');
 
-test("returns I if passed 1 as an argument", function () {
-  // Arrange
-  const number = 1;
-  const expected = "I";
-
-  // Act
-  const result = convertToOldRoman(number);
-
-  // Assert
-  expect(result).toEqual(expected);
+test('returns I if passed 1 as an argument', () => {
+  expect(convertToOldRoman(1)).toEqual('I');
 });
+
+test('returns IV if passed 4 as an argument', () => {
+  expect(convertToOldRoman(4)).toEqual('IV');
+});
+
+test('returns IX if passed 9 as an argument', () => {
+  expect(convertToOldRoman(9)).toEqual('IX');
+});
+
+test('returns XL if passed 40 as an argument', () => {
+  expect(convertToOldRoman(40)).toEqual('XL');
+});
+
+test('returns XC if passed 90 as an argument', () => {
+  expect(convertToOldRoman(90)).toEqual('XC');
+});
+
+
+
+
+
