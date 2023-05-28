313d07de5dfa0b67918cd8ac70670b5d6c49b822
fowobi <109865505+fowobi@users.noreply.github.com>
2023-05-28 09:13:12 +0100

    update dog-photo-gallery

--- dog-photo-gallery/script.js
+++ dog-photo-gallery/script.js
@@ -1,26 +1,27 @@
 async function randomDogPicture (){
    try {
-       const response = await fetch('https://dog.ceo/api/breeds/image/random')
-       const json = await response.json()
-       return json.message
+       const response = await fetch('https://dog.ceo/api/breeds/image/random');
+       const json = await response.json();
+       return json.message;
       
    } catch (error) {
-       return error
+    //    return error
+     throw new Error(error);
  }
 }
 
 
 const button = document.getElementById('btn')
 button.addEventListener('click', async () => {
-   const list = document.getElementById('list')
+   const list = document.getElementById('list');
 
 
- const item = document.createElement('li')
- list.appendChild(item)
+ const item = document.createElement('li');
+ list.appendChild(item);
 
 
- const picture = document.createElement('img')
- const pictureUrl = await randomDogPicture()
- picture.src = pictureUrl
- item.appendChild(picture)
-})
+ const picture = document.createElement('img');
+ const pictureUrl = await randomDogPicture();
+ picture.src = pictureUrl;
+ item.appendChild(picture);
+});
