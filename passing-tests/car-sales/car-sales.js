6d42658d46244b3e0901b5038f4c591b0f6ce0d0
fowobi <109865505+fowobi@users.noreply.github.com>
2023-05-28 09:59:46 +0100

    Update car-sales.js

--- passing-tests/car-sales/car-sales.js
+++ passing-tests/car-sales/car-sales.js
@@ -1,3 +1,17 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+    let totals = {};
+
+  carsSold.forEach((car) => {
+    const { make, price } = car;
+
+    if (totals[make]) {
+      totals[make] += price;
+    } else {
+      totals[make] = price;
+    }
+  });
+
+  return totals;
+}
 
 module.exports = sales;
