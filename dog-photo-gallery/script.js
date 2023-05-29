94671ab6e3099a5f0ad5955cee3c6d72a48f230b
Irianni Munoz <82213740+munozirianni1988@users.noreply.github.com>
2023-05-29 21:18:22 +0100

    Update script.js

--- dog-photo-gallery/script.js
+++ dog-photo-gallery/script.js
@@ -1,6 +1,8 @@
 let divElement = document.createElement("div");
 let ulElement = document.createElement("ul");
 divElement.appendChild(ulElement);
+let liElement = document.createElement("li");
+let imgElement = document.createElement("img");
 let buttonElement = document.createElement("button");
 buttonElement.innerText = "Click Me!";
 divElement.appendChild(buttonElement);
@@ -11,11 +13,10 @@ buttonElement.addEventListener("click", fetchImage);
 function fetchImage() {
   fetch("https://dog.ceo/api/breeds/image/random")
     .then((response) => {
-      return response.json;
+      return response.json();
     })
     .then((data) => {
-      let liElement = document.createElement("li");
-      let imgElement = document.createElement("img");
+      console.log(data);
       imgElement.src = data.message;
       liElement.appendChild(imgElement);
       ulElement.appendChild(liElement);
