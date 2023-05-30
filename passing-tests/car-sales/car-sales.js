206e386d73790030a8615ce1710e04c915318150
Bedi Omuri <113227370+Bedi06@users.noreply.github.com>
2023-05-31 00:40:00 +0100

    passing-tests done

--- passing-tests/car-sales/car-sales.js
+++ passing-tests/car-sales/car-sales.js
@@ -1,3 +1,19 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+    let totals={};
+
+    for(let i=0 ;i < carsSold.length;i++ ){
+        let car = carsSold[i];
+        let make = car.make;
+
+        if (totals[make]) {
+            totals[make] += car.price;
+            
+        } else {
+            totals[make] = car.price;
+            
+        }
+    }
+    return totals;
+}
 
 module.exports = sales;
--- passing-tests/factorial/factorial.js
+++ passing-tests/factorial/factorial.js
@@ -8,6 +8,13 @@
 // calculate and return the factorial of int
 // note: factorial of 0 is 1
 
-function factorial(int) {}
+function factorial(int) {
+if ( int=== 0|| int === 1){
+    return 1;
+}else {
+    return int * factorial(int-1);
+}
+
+}
 
 module.exports = factorial;
