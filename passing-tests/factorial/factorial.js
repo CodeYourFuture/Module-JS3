d8370a5eacb0f4aaaf0d00fa096bb93b8ce602be
Haroun Alarabi <alarabiharoun@gmail.com>
2023-05-30 21:37:56 +0100

    Unit-test solved

--- passing-tests/factorial/factorial.js
+++ passing-tests/factorial/factorial.js
@@ -8,6 +8,12 @@
 // calculate and return the factorial of int
 // note: factorial of 0 is 1
 
-function factorial(int) {}
+function factorial(int) {
+    if (int === 0) {
+        return 1;
+        }
+        return int * factorial(int - 1);
+        
+}
 
 module.exports = factorial;
