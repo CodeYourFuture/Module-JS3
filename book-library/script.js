de86f60383c10ca4de818e6e7663f0e505f65203
Afsha10 <115444059+Afsha10@users.noreply.github.com>
2023-06-05 01:43:34 +0100

    Made some small changes

--- book-library/script.js
+++ book-library/script.js
@@ -8,7 +8,12 @@ window.addEventListener("load", function (e) {
 function populateStorage() {
   if (myLibrary.length !== 0) {
     let book1 = ["Robison Crusoe", "Daniel Defoe", "252", true];
-    let book2 = [ "The Old Man and the Sea", "Ernest Hemingway", "127",true];
+    let book2 = [
+      "The Old Man and the Sea",
+      "Ernest Hemingway",
+      "127",
+      true
+    ];
     myLibrary.push(book1);
     myLibrary.push(book2);
     render();
@@ -20,8 +25,8 @@ const author = document.getElementById("author");
 const pages = document.getElementById("pages");
 const check = document.getElementById("check");
 
-//check the right input from forms and if its ok -> add the new book (object in array)
-//via Book function and start render function
+// check the right input from forms and if its ok -> add the new book (object in array)
+// via Book function and start render function
 function submit() {
   if (
     title.value == null ||
@@ -38,7 +43,7 @@ function submit() {
   }
 }
 
-function book(title, author, pages, check) {
+function book({title, author, pages, check}) {
   this.title = title;
   this.author = author;
   this.pages = pages;
