eeeee560335066fd59b11908c48d020a208df87a
AppolinFotso <appolin.s.fotso@gmail.com>
2023-06-07 14:11:49 +0100

    Updated Comic function

--- programmer-humour/index.js
+++ programmer-humour/index.js
@@ -1,16 +1,11 @@
 
 const image = document.querySelector('img')
-async function comic() {
-    try {
+function comic() {
     
-        const promiseData = await fetch('https://xkcd.now.sh/?comic=latest')
-        const data = await promiseData.json()
-        console.log(data)
-image.setAttribute('src', `${data.img}`)
-}
-    catch(error){
-        (console.error(error))
-    }
+    
+        const promiseData = fetch('https://xkcd.now.sh/?comic=latest')
+        promiseData.then(response=> response.json()).then(data=>image.setAttribute('src', `${data.img}`)).catch(error=>console.error(error))
+    
 
 }
 
