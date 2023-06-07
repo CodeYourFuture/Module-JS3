f10d3979e3d2a7a45b2fd6047d73e56fbcca6ec8
AdrianIlovan <adrian88ilovan@yahoo.com>
2023-06-07 14:32:12 +0100

    Exercise 1 Done

--- array-destructuring/exercise-1/exercise.js
+++ array-destructuring/exercise-1/exercise.js
@@ -4,10 +4,15 @@ const personOne = {
   favouriteFood: "Spinach",
 };
 
-function introduceYourself(___________________________) {
+
+
+function introduceYourself(personOne) {
+  let {name, age, favouriteFood} = personOne;
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
 }
 
-introduceYourself(personOne);
+introduceYourself();
+
+
