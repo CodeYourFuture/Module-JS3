f03da2a3077d87e3f727fec4078f958abb9f52f9
malkitbenning <malkitbenning@gmail.com>
2023-06-07 21:47:19 +0100

    completed unit test exercises

--- passing-tests/get-average/get-average.js
+++ passing-tests/get-average/get-average.js
@@ -2,6 +2,14 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+    let onlyNumbers = numbers.filter((entry) => {
+       return (typeof entry === "number")
+    })
+    let total = onlyNumbers.reduce ((accumulator, aNumber) => {
+        return accumulator + aNumber;
+    }, 0)
+    return average = total / onlyNumbers.length;
+};
 
 module.exports = average;
