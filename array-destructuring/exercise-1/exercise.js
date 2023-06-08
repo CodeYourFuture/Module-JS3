5fe3103b2a477e854c5a9526fd8baae7adee5bcc
Afsha10 <115444059+Afsha10@users.noreply.github.com>
2023-06-08 21:56:31 +0100

    Refactored by removing the default value and just passing the object as an argument.

--- array-destructuring/exercise-1/exercise.js
+++ array-destructuring/exercise-1/exercise.js
@@ -5,7 +5,7 @@ const personOne = {
 };
 
 
-function introduceYourself({ name, age, favouriteFood } = personOne) {
+function introduceYourself({ name, age, favouriteFood }) {
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
