5e6800c3f083b16684d99daea99c2a0052d2a1ae
AdrianIlovan <adrian88ilovan@yahoo.com>
2023-06-08 16:16:14 +0100

    Car-Sales Done

--- passing-tests/car-sales/car-sales.js
+++ passing-tests/car-sales/car-sales.js
@@ -1,3 +1,14 @@
-function sales(carsSold) {}
-
+function sales(carsSold) {
+    let totals = {}
+    
+    for (let car of carsSold) {
+       let {make, price} = car;
+    if (totals.hasOwnProperty(make)) {
+        totals[make] += price;
+    } else {
+        totals[make] = price;
+    }
+}
+    return totals;
+}
 module.exports = sales;
