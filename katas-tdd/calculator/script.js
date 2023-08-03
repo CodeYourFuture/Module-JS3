7cf4b04a14731319c6a5e8c056f6e3d714d66211
batuncer <tuncerbaki@gmail.com>
2023-08-03 18:42:48 +0100

    calculator is done

--- katas-tdd/calculator/script.js
+++ katas-tdd/calculator/script.js
@@ -1,9 +1,25 @@
-function add(numbers){
-    const numberArray = numbers.split(",")
+function add(numbers) {
+  if (numbers === "") {
+    return 0;
+  }
 
-    const parsedNumvers =numberArray.map((num)=> parseFloat(num))
+  const numberArray = numbers.split(",");
+  const parsedNumbers = numberArray.map((num) => parseFloat(num));
 
-    const sum = parsedNumvers.reduce((a, b) => a+b)
+  // Filter out numbers greater than 1000
+  const validNumbers = parsedNumbers.filter((num) => num <= 1000);
 
-    return sum
+  // Check for negative numbers
+  const negativeNumbers = validNumbers.filter((num) => num < 0);
+  if (negativeNumbers.length > 0) {
+    throw new Error(`Negatives not allowed: ${negativeNumbers.join(", ")}`);
+  }
+
+  return validNumbers.reduce((x, y) => x + y);
 }
+
+let numbers = "3,4,6,2000"
+console.log(add(numbers))
+
+let numbersTwo = "2,3,5,-1,-2"
+console.log(add(numbersTwo))
