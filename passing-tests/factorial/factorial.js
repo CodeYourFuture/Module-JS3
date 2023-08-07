de6c41155e8a6c5210f2b3764d2f7396064a7f20
batuncer <tuncerbaki@gmail.com>
2023-08-07 15:52:52 +0100

    passing test file is done

--- passing-tests/factorial/factorial.js
+++ passing-tests/factorial/factorial.js
@@ -8,6 +8,17 @@
 // calculate and return the factorial of int
 // note: factorial of 0 is 1
 
-function factorial(int) {}
+function factorial(int) {
+
+if (int === 0) {
+    return 1;
+  } else {
+    let result = 1;
+    for (let i = 1; i <= int; i++) {
+      result *= i;
+    }
+    return result;
+  }
+}
 
 module.exports = factorial;
