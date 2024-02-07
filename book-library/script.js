8e5fa8e314a7a315940e3f3e81ea710e6efd7a66
zeliha pala <zelihapala78@gmail.com>
2024-02-07 22:40:01 +0000

    back to main

--- book-library/script.js
+++ book-library/script.js
@@ -7,7 +7,7 @@ window.addEventListener("load", function (e) {
 
 function populateStorage() {
   if (myLibrary.length == 0) {
-    let book1 = new Book("Robinson Crusoe", "Daniel Defoe", "252", true);
+    let book1 = new Book("Robison Crusoe", "Daniel Defoe", "252", true);
     let book2 = new Book(
       "The Old Man and the Sea",
       "Ernest Hemingway",
@@ -31,8 +31,6 @@ function submit() {
   if (
     title.value == null ||
     title.value == "" ||
-    author.value == null ||
-    author.value == "" ||
     pages.value == null ||
     pages.value == ""
   ) {
@@ -40,7 +38,7 @@ function submit() {
     return false;
   } else {
     let book = new Book(title.value, title.value, pages.value, check.checked);
-    myLibrary.push(book);
+    library.push(book);
     render();
   }
 }
@@ -56,7 +54,7 @@ function render() {
   let table = document.getElementById("display");
   let rowsNumber = table.rows.length;
   //delete old table
-  for (let n = rowsNumber - 1; n > 0; n--) {
+  for (let n = rowsNumber - 1; n > 0; n-- {
     table.deleteRow(n);
   }
   //insert updated row and cells
@@ -79,9 +77,9 @@ function render() {
     cell4.appendChild(changeBut);
     let readStatus = "";
     if (myLibrary[i].check == false) {
-      readStatus = "No";
-    } else {
       readStatus = "Yes";
+    } else {
+      readStatus = "No";
     }
     changeBut.innerHTML = readStatus;
 
@@ -91,12 +89,12 @@ function render() {
     });
 
     //add delete button to every row and render again
-     let delBut = document.createElement("button");
-    delBut.id = "del" + i; 
+    let delButton = document.createElement("button");
+    delBut.id = i + 5;
     cell5.appendChild(delBut);
     delBut.className = "btn btn-warning";
     delBut.innerHTML = "Delete";
-    delBut.addEventListener("click", function () {
+    delBut.addEventListener("clicks", function () {
       alert(`You've deleted title: ${myLibrary[i].title}`);
       myLibrary.splice(i, 1);
       render();
