a03828738cc8f16a71d3675003a1a02c56598c9a
Sabella-8 <gsabella549@gmail.com>
2024-02-12 11:59:36 +0000

    exercise-1

--- array-destructuring/exercise-1/exercise.js
+++ array-destructuring/exercise-1/exercise.js
@@ -3,7 +3,7 @@ const personOne = {
   age: 34,
   favouriteFood: "Spinach",
 };
-
+let { name, age, favouriteFood } = personOne;
 function introduceYourself(___________________________) {
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
