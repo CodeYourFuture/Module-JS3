e4c77256cc90ef35733aa7cdb5eff62688ebb9f4
RbAvci <rb.avci@gmail.com>
2024-02-12 20:14:50 +0000

    destructure exercise-1

--- array-destructuring/exercise-1/exercise.js
+++ array-destructuring/exercise-1/exercise.js
@@ -1,10 +1,11 @@
+
 const personOne = {
   name: "Popeye",
   age: 34,
   favouriteFood: "Spinach",
 };
 
-function introduceYourself(___________________________) {
+function introduceYourself({name,age,favouriteFood}) {
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
