8afb2772783ad065272bc1bcd6248f782b9df63b
Your Name <you@example.com>
2024-02-14 22:30:07 +0000

    finished all 3

--- array-destructuring/exercise-3/exercise.js
+++ array-destructuring/exercise-3/exercise.js
@@ -6,3 +6,13 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+let sum = 0; 
+
+order.forEach(({ itemName, quantity, unitPrice }) => {
+  let total = unitPrice*quantity;
+  console.log(`${quantity} ${itemName} ${total.toFixed(2)}`);
+  sum += total;
+});
+
+console.log(`Total: ${sum}`);
