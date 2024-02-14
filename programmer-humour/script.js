189c521f89dea614cf5f84b7c36f74270723fd53
Bakhat Begum <bakhtbagum@gmail.com>
2024-02-14 16:26:53 +0000

    added html, script and style files, using fetch to a request rom API

---
+++ programmer-humour/script.js
@@ -0,0 +1,31 @@
+
+function getImageFetch(){
+  return fetch('https://xkcd.now.sh/?comic=latest').then((response) => {
+    if(!response.ok){
+        return "Error";
+      }else{
+        return response.json();
+      }
+  });
+}
+getImageFetch();
+
+let imageDiv = document.createElement('div');
+document.body.appendChild(imageDiv);
+
+function displayData(){
+    getImageFetch().then((data) => {
+        if(!data){
+            console.log("data can not found");
+        }
+        const image = data.img;
+
+        const createImage = document.createElement('img');
+        createImage.src = image;
+        imageDiv.appendChild(createImage);
+    } )
+}
+console.log(displayData());
+
+window.onload = displayData;
+
