4792d9ec54cfc63fed9ab51c775c961f73110d88
Sabella-8 <gsabella549@gmail.com>
2024-02-14 17:38:26 +0000

    comic image

---
+++ programmer-humour/script.js
@@ -0,0 +1,19 @@
+const imageComic = document.getElementById("imageComic");
+
+function fetching() {
+  return fetch("https://xkcd.now.sh/?comic=latest")
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("error");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      imageComic.src = data.img;
+    })
+    .catch((error) => {
+      console.error("Error fetching comic:");
+    });
+}
+fetching();
