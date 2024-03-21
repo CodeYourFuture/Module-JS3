4c6d78c6d2cddc203d5180ac30d68519e1e3057c
Zeliha Pala <zelihapala78@gmail.com>
2024-03-21 00:56:01 +0000

    updated

--- programmer-humour/script.js
+++ programmer-humour/script.js
@@ -5,6 +5,7 @@ fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
         //header
+        
         const h1Humor = document.createElement('h1');
         h1Humor.textContent = 'Program Humor';
         //title
@@ -14,6 +15,7 @@ fetch(apiUrl)
         //img for displayin comic
         const img = document.createElement('img');
         img.src = data.img;
+      
         img.alt = data.alt;
 
         comicContainer.appendChild(h1Humor);
@@ -25,6 +27,7 @@ fetch(apiUrl)
         comicContainer.appendChild(hr);
 
         // a p for text
+        
         const moonArmorText = document.createElement('p');
         moonArmorText.textContent = 'Moon Landing Mission Profiles';
         comicContainer.appendChild(moonArmorText);
@@ -46,48 +49,48 @@ fetch(apiUrl)
         hiddenP.id = 'moon'; // id for easy access
         comicContainer.appendChild(hiddenP);
 
- // a div for buttons  especially for css
- const buttonsDiv = document.createElement('div');
- buttonsDiv.classList.add("btndiv") 
+        // a div for buttons  especially for css
+        const buttonsDiv = document.createElement('div');
+        buttonsDiv.classList.add("btndiv") 
 
- //Add 3 buttons
- const archiveButton = document.createElement('button');
- archiveButton.textContent = 'ARCHIVE';
- archiveButton.onclick = function() {
-     window.location.href = 'https://m.xkcd.com/archive/';
- };
+         //Add 3 buttons
+        const archiveButton = document.createElement('button');
+        archiveButton.textContent = 'ARCHIVE';
+        archiveButton.onclick = function() {
+        window.location.href = 'https://m.xkcd.com/archive/';
+        };
 
- const whatIfButton = document.createElement('button');
- whatIfButton.textContent = 'WHAT IF??';
- whatIfButton.onclick = function() {
-     window.location.href = 'https://what-if.xkcd.com/';
- };
+        const whatIfButton = document.createElement('button');
+        whatIfButton.textContent = 'WHAT IF??';
+        whatIfButton.onclick = function() {
+        window.location.href = 'https://what-if.xkcd.com/';
+        };
 
- const aboutButton = document.createElement('button');
- aboutButton.textContent = 'ABOUT';
- aboutButton.onclick = function() {
-     window.location.href = 'https://xkcd.com/about/';
- };
+        const aboutButton = document.createElement('button');
+        aboutButton.textContent = 'ABOUT';
+        aboutButton.onclick = function() {
+        window.location.href = 'https://xkcd.com/about/';
+        };
 
- // Append buttons to the div
- buttonsDiv.appendChild(archiveButton);
- buttonsDiv.appendChild(whatIfButton);
- buttonsDiv.appendChild(aboutButton);
+        // Append buttons to the div
+        buttonsDiv.appendChild(archiveButton);
+        buttonsDiv.appendChild(whatIfButton);
+        buttonsDiv.appendChild(aboutButton);
 
- // Append div to the comic container
- comicContainer.appendChild(buttonsDiv);
-// ADD FOOTER
-const footer = document.createElement('footer');
-const link = document.createElement('a');
-link.textContent = 'Visit xkcd and have fun';
-link.href = 'https://m.xkcd.com/';
-link.target = '_blank'; //new tab goes to original website
+        // Append div to the comic container
+        comicContainer.appendChild(buttonsDiv);
+        // ADD FOOTER
+        const footer = document.createElement('footer');
+        const link = document.createElement('a');
+        link.textContent = 'Visit xkcd and have fun';
+        link.href = 'https://m.xkcd.com/';
+        link.target = '_blank'; //new tab goes to original website
 
-// Append link to the footer
-footer.appendChild(link);
+        // Append link to the footer
+        footer.appendChild(link);
 
-// Append footer to the comic container
-comicContainer.appendChild(footer);
+        // Append footer to the comic container
+        comicContainer.appendChild(footer);
 
     })
     .catch(error => {
