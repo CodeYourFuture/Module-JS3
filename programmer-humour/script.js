bb83c05b73a1e0b1c8dfa26faf96878e6742acf5
Zeliha Pala <zelihapala78@gmail.com>
2024-03-21 00:38:18 +0000

    Added JS code to fetch and display the latest XKCD comic with header and title

---
+++ programmer-humour/script.js
@@ -0,0 +1,27 @@
+const apiUrl = 'https://xkcd.now.sh/?comic=latest';
+const comicContainer = document.getElementById('comic-container');
+
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        //header
+        const h1Humor = document.createElement('h1');
+        h1Humor.textContent = 'Program Humor';
+        //title
+        const p = document.createElement('p');
+        p.textContent = 'Who knew programmers could be funny?';
+
+        //img for displayin comic
+        const img = document.createElement('img');
+        img.src = data.img;
+        img.alt = data.alt;
+
+        comicContainer.appendChild(h1Humor);
+        comicContainer.appendChild(p);
+        comicContainer.appendChild(img);
+
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+        comicContainer.textContent = 'Error loading comic. Try again later.';
+    });
