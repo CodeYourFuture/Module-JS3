3c58f2f59092745598df4a606eeb866b38980748
Nazanin Saedi <niliom1988@gmail.com>
2024-05-01 22:10:33 +0100

    fetch & render

--- programmer-humour/script.js
+++ programmer-humour/script.js
@@ -1,5 +1,16 @@
 async function fetchComic(){
     try {
+        const response = await fetch('https://xkcd.now.sh/?comic=latest');
+        const data = await response.json();
         
+        // Log the received data to the console
+        console.log(data);
+
+        //render the comic img
+        renderComic(data.img);
+    } catch (error) {
+        //handle errors
+        console.error('Error fetching comic:', error);
+    }
     }
 }
