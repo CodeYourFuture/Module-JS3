3efbad2acb89e012a74c3bc7810b0d23d52da2e7
Elmira Zangeneh <elmira.zangeneh89@gmail.com>
2024-05-01 16:34:09 +0100

    fix delete button

--- book-library/script.js
+++ book-library/script.js
@@ -94,7 +94,7 @@ function render() {
     cell5.appendChild(delButton);
     delButton.className = "btn btn-warning";
     delButton.innerHTML = "Delete";
-    delButton.addEventListener("clicks", function () {
+    delButton.addEventListener("click", function () {
       alert(`You've deleted title: ${myLibrary[i].title}`);
       myLibrary.splice(i, 1);
       render();
