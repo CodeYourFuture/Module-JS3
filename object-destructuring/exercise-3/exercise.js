de16492441e4988884e6916b411057e8976e4c4b
Nazanin Saedi <niliom1988@gmail.com>
2024-05-03 20:43:49 +0100

    addfunctions

--- object-destructuring/exercise-3/exercise.js
+++ object-destructuring/exercise-3/exercise.js
@@ -1,3 +1,4 @@
+// Define the order as an array of objects, each object representing an item
 let order = [
   { itemName: "Hot cakes", quantity: 1, unitPrice: 2.29 },
   { itemName: "Apple Pie", quantity: 2, unitPrice: 1.39 },
@@ -6,3 +7,24 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+//function to calculate the total cost of an item
+function calculateTotal(quantity, unitPrice) {
+  return quantity * unitPrice;
+}
+
+//function to print the receipt
+function printReceipt(order) {
+  console.log("QTY\tItem\t\t\tTOTAL");
+
+  let totalOrderCost = 0;
+
+  order.array.forEach(element => {
+    const total = calculateTotal(item.quantity, item.unitPrice);
+
+    totalOrderCost += total;
+    
+  });
+
+
+}
